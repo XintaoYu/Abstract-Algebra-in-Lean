9cba04805ac52aba63e610ba1951297699ae3f70
junemily <161188399+junemily@users.noreply.github.com>
2024-03-10 16:56:14 +0800

    changes on 13

--- AbstractAlgebraInLean/Exercise/Ex13.lean
+++ AbstractAlgebraInLean/Exercise/Ex13.lean
@@ -1,22 +1,24 @@
 import Mathlib.Tactic
 import Mathlib.Algebra.Group.Basic
 import Mathlib.Init.Function
--- Suppose G is a group, a is an element in G
+-- Suppose G is a group, a is an element in G.
 variable  [Group G] (a : G)
---Define f x that equals to a*x
+--Define f x that equals to a*x.
 def f (x : G) : G := a * x
 
 example : Function.Injective (f a) ∧ Function.Surjective (f a) := by
   constructor
-  --To prove f is injective
-  · intro x y h
+  --To prove f is injective.
+  ·--if f is injective, f x = f y implies x = y.
+    intro x y h
     rw [f] at h
     rw [f] at h
     apply mul_left_cancel h
-    
-  --To prove f is surjective
-  · intro b
-    -- use a⁻¹*b in G since a and b are elements in G
+
+  --To prove f is surjective.
+  · --if f is surjective,every b is equal to f a .
+    intro b
+    -- use a⁻¹*b in G since a and b are elements in G.
     use a⁻¹*b
     rw [f]
     rw [← mul_assoc]
