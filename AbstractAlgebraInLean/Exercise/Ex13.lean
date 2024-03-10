d04b16512f47276dd37ea15a5b787e96a38a8fcb
junemily <161188399+junemily@users.noreply.github.com>
2024-03-10 17:17:54 +0800

    change on ex13

--- AbstractAlgebraInLean/Exercise/Ex13.lean
+++ AbstractAlgebraInLean/Exercise/Ex13.lean
@@ -3,37 +3,23 @@ import Mathlib.Algebra.Group.Basic
 import Mathlib.Init.Function
 -- Suppose G is a group, a is an element in G.
 variable  [Group G] (a : G)
---Define f x that equals to a*x.
+-- Define f x that equals to a*x.
 def f (x : G) : G := a * x
-
-example : Function.Injective (f a) ∧ Function.Surjective (f a) := by
+-- To prove f is bijective.
+example : (f a).Bijective := by
   constructor
-  --To prove f is injective.
-  ·--if f is injective, f x = f y implies x = y.
+  -- To prove f is injective.
+  ·-- f x = f y ,that is a*x = a*y , implies x = y by mul_left_cancel.
     intro x y h
     rw [f] at h
     rw [f] at h
     apply mul_left_cancel h
 
   --To prove f is surjective.
-  · --if f is surjective,every b is equal to f a .
+  · -- It suffices to show that every b is equal to f a .
     intro b
-    -- use a⁻¹*b in G since a and b are elements in G.
+    -- f (a⁻¹*b) = a * (a⁻¹ * b) = a * a⁻¹ * b = b
     use a⁻¹*b
     rw [f]
     rw [← mul_assoc]
     rw [mul_right_inv, one_mul]
-
-
-
-
-
-
-
-
-
-
-
-
-
-  · intro
